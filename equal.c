6c6a79a8aa1dc1fc2836c2c9b2c2da9efc0490b0
aditya-20012 <adityaga9899@gmail.com>
2026-02-18 21:41:58 +0530

    Add files via upload

---
+++ equal.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter number 1:");
+    scanf("%d",&a);
+    printf("Enter number 2:");
+    scanf("%d",&b);
+    if (a==b){
+        printf("Equal");
+
+    }else{
+        printf("Not equal");
+
+    }
+    return 0;
+}
+    
+
+        
