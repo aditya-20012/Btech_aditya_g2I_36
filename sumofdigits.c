aefe67a925820a085d56394d96ff7f2af1d7ee3f
aditya-20012 <adityaga9899@gmail.com>
2026-02-19 10:46:14 +0530

    Add files via upload

---
+++ sumofdigits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,s=0,a=0;
+    printf("Enter number to find sum of digits:");
+    scanf("%d",&n);
+    for (;n!=0;){
+        a=n%10;
+        s=s+a;
+        n=n/10;
+    }
+    printf("Sum of digits:%d",s);
+    }
+        
