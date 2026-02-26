0bdf8fe2321d714451281cad2a91843c6a5ca62f
aditya-20012 <adityaga9899@gmail.com>
2026-02-26 11:36:02 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,reverse=0,a=0;
+    printf("Enter number to find reverse:");
+    scanf("%d",&n);
+    int d=n;
+    for(;n!=0;){
+        a=n%10;
+        reverse=reverse*10+a;
+        n=n/10;
+
+    }if(d==reverse){
+        printf("Palindrome");
+    }
+}
